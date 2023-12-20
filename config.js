5536160d4c1c3e6c588822536b5e6b1f763e88c7
Kizzy <154281068+wKizzy@users.noreply.github.com>
2023-12-21 00:08:57 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "prefix": ".",
+  "sahip": "İD'NİZ",
+  "token": "TOKEN",
+  "durum": "Developer By Kizzy | O L Y M P O S",
+  "Footer": "Developer By Kizzy | O L Y M P O S",
+  
+  "etkinlikKatılımcısı": "",
+  "cekilisKatılımcısı": "",
+  "duyurubildirim": "",
+  
+  "kirmizi": "🍓",
+  "mavi": "🌊",
+  "yesil": "🌵",
+  "mor": "🍇",
+  "pembe": "🌸",
+  "kahverengi": "🪵",
+  "siyah": "🏴",
+  "beyaz": "☁️",
+
+   "sevgilimvar": "Sevgilim Var 💍",
+   "sevgilimyok": "Sevgilim Yok 💔",
+   "nomanit": "No Manit No Dırdır 🤍",
+   "lgbt": "LGBT 🏳️‍🌈",
+   
+    "koç": "Koç | ♈",
+    "boğa": "Boğa | ♉",
+    "ikizler": "İkizler | ♊",
+    "yengeç": "Yengeç | ♋",
+    "aslan": "Aslan | ♌",
+    "başak": "Başak | ♍",
+    "terazi": "Terazi | ♎",
+    "akrep": "Akrep | ♏",
+    "yay": "Yay | ♐",
+    "oğlak": "Oğlak | ♑",
+    "kova": "Kova | ♒",
+    "balık": "Balık | ♓"
+}
